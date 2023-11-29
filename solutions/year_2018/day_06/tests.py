745820d74536e663e2e2bbd31bf3d2d3940addf0
Kenny <kfonatoko@gmail.com>
2023-11-29 16:36:09 -0500

    Fix 2018 day 06 test

--- solutions/year_2018/day_06/tests.py
+++ solutions/year_2018/day_06/tests.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from solutions import BaseTestCase
 from solutions.year_2018.day_06.solution import Year2018Day06
 
@@ -12,3 +14,7 @@ class TestSample(BaseTestCase):
     def setUp(self):
         self.solution = Year2018Day06(__file__, "sample.txt")
         self.answers = (17, 16)
+
+    @patch('solutions.year_2018.day_06.solution.PART_2_TARGET_DISTANCE', 32)
+    def test_part_2(self):
+        return super().test_part_2()
