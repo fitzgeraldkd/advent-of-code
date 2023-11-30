9815be8960a1453e0a4a63f4647383078da15200
Kenny <kfonatoko@gmail.com>
2023-11-29 21:44:58 -0500

    Run black on code

--- solutions/year_2018/day_06/tests.py
+++ solutions/year_2018/day_06/tests.py
@@ -15,6 +15,6 @@ class TestSample(BaseTestCase):
         self.solution = Year2018Day06(__file__, "sample.txt")
         self.answers = (17, 16)
 
-    @patch('solutions.year_2018.day_06.solution.PART_2_TARGET_DISTANCE', 32)
+    @patch("solutions.year_2018.day_06.solution.PART_2_TARGET_DISTANCE", 32)
     def test_part_2(self):
         return super().test_part_2()
