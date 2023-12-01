613536f1e45bdff6142d27c9d2f1d70ae63a9409
Kenny <kfonatoko@gmail.com>
2023-11-30 20:17:40 -0500

    Complete year 2020 day 09

---
+++ solutions/year_2020/day_09/solution.py
@@ -0,0 +1,37 @@
+from solutions import BaseSolution
+
+
+class Year2020Day09(BaseSolution):
+    PREAMBLE = 25
+
+    def _parse_line(self, line: str):
+        return int(line.strip())
+
+    def part_1(self):
+        xmas = self.inputs
+        for i in range(self.PREAMBLE, len(xmas)):
+            target = xmas[i]
+            sub_xmas = xmas[i - self.PREAMBLE : i]
+            values = set(sub_xmas)
+            value = next((value for value in values if target - value in values), None)
+            if not value or (value * 2 == target and sub_xmas.count(value) == 1):
+                return target
+
+    def part_2(self):
+        xmas = self.inputs
+        target = self.part_1()
+        start, end = 0, 1
+
+        while True:
+            sub_xmas = xmas[start:end]
+            aggregate = sum(sub_xmas)
+            if aggregate < target:
+                end += 1
+            elif aggregate > target:
+                start += 1
+            else:
+                return min(sub_xmas) + max(sub_xmas)
+
+
+if __name__ == "__main__":
+    Year2020Day09(__file__).print_results()
---
+++ solutions/year_2020/day_09/tests.py
@@ -0,0 +1,15 @@
+from solutions import BaseTestCase
+from solutions.year_2020.day_09.solution import Year2020Day09
+
+
+class TestSolution(BaseTestCase):
+    def setUp(self):
+        self.solution = Year2020Day09(__file__)
+        self.answers = (258585477, 36981213)
+
+
+class TestSample(BaseTestCase):
+    def setUp(self):
+        self.solution = Year2020Day09(__file__, "sample.txt")
+        self.solution.PREAMBLE = 5
+        self.answers = (127, 62)
