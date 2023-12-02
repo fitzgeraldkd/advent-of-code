549c96210d833cd0007157db86f3eebaba36b1f4
Kenny <kfonatoko@gmail.com>
2023-12-02 01:36:47 -0500

    Complete year 2020 day 13

---
+++ solutions/year_2020/day_13/solution.py
@@ -0,0 +1,45 @@
+import numpy
+
+from solutions import BaseSolution
+
+
+class Year2020Day13(BaseSolution):
+    def _parse_inputs(self):
+        lines = super()._parse_inputs()
+        return int(lines[0]), [int(n) if n != "x" else n for n in lines[1].split(",")]
+
+    def part_1(self):
+        timestamp, buses = self.inputs
+
+        delays = [(bus, (-1 * timestamp) % bus) for bus in buses if bus != "x"]
+        first_bus = min(delays, key=lambda delay: delay[1])
+        return first_bus[0] * first_bus[1]
+
+    def part_2(self):
+        _, buses = self.inputs
+
+        timestamp = 0
+        for l in range(1, len(buses)):
+            subset = buses[: l + 1]
+            lcm = numpy.lcm.reduce(list(filter(lambda bus: bus != "x", subset[:-1])))
+
+            while True:
+                valid = True
+                for i, bus in enumerate(subset):
+                    if bus == "x":
+                        continue
+
+                    if 0 != (-1 * (timestamp + i)) % bus:
+                        valid = False
+                        break
+
+                if valid:
+                    break
+
+                timestamp += lcm
+
+        return timestamp
+
+
+if __name__ == "__main__":
+    Year2020Day13(__file__).print_results()
