e8257b88f053708146aea7b541e0d6766eaf3b78
Kenny <kfonatoko@gmail.com>
2023-12-04 00:50:02 -0500

    Refactor year 2023 day 04

--- solutions/year_2023/day_04/solution.py
+++ solutions/year_2023/day_04/solution.py
@@ -9,36 +9,20 @@ class Year2023Day04(BaseSolution):
 
     def _parse_line(self, line: str):
         _, numbers = line.strip().split(": ")
-        winning_numbers, card_numbers = numbers.split(" | ")
-        return (
-            {int(n) for n in re.findall(r"\d+", winning_numbers)},
-            {int(n) for n in re.findall(r"\d+", card_numbers)},
-        )
-
-    def get_card_wins(self, winning_numbers: Set[int], card_numbers: Set[int]) -> int:
+        winning_numbers, card_numbers = [
+            {int(v) for v in re.findall(r"\d+", n)} for n in numbers.split(" | ")
+        ]
         return len(winning_numbers.intersection(card_numbers))
 
     def part_1(self):
-        score = 0
-
-        for winning_numbers, card_numbers in self.inputs:
-            card_wins = self.get_card_wins(winning_numbers, card_numbers)
-            if card_wins > 0:
-                score += 2 ** (card_wins - 1)
-
-        return score
+        return sum(0 if wins == 0 else (2 ** (wins - 1)) for wins in self.inputs)
 
     def part_2(self):
         cards = self.inputs
         card_counts = [1 for _ in range(len(cards))]
 
-        for i, (winning_numbers, card_numbers) in enumerate(cards):
-            card_wins = self.get_card_wins(winning_numbers, card_numbers)
-
-            if card_counts[i] == 1 and card_counts == 0:
-                break
-
-            for j in range(i + 1, i + 1 + card_wins):
+        for i, wins in enumerate(cards):
+            for j in range(i + 1, i + 1 + wins):
                 card_counts[j] += card_counts[i]
 
         return sum(card_counts)
