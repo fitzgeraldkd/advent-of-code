ebb814dc8cb0e10a10df6bf99cce76e125ff0389
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2024-01-08 17:23:31 -0500

    Add sample tests for year 2016 days 1 through 5

--- solutions/year_2016/day_02/tests.py
+++ solutions/year_2016/day_02/tests.py
@@ -6,3 +6,9 @@ class TestSolution(BaseTestCase):
     def setUp(self):
         self.solution = Year2016Day02()
         self.answers = ('47978', '659AD')
+
+
+class TestSample(BaseTestCase):
+    def setUp(self):
+        self.solution = Year2016Day02("sample.txt")
+        self.answers = ('1985', '5DB3')
--- solutions/year_2016/day_05/tests.py
+++ solutions/year_2016/day_05/tests.py
@@ -6,3 +6,9 @@ class TestSolution(BaseTestCase):
     def setUp(self):
         self.solution = Year2016Day05()
         self.answers = ('f97c354d', '863dde27')
+
+
+class TestSample(BaseTestCase):
+    def setUp(self):
+        self.solution = Year2016Day05('sample.txt')
+        self.answers = ('18f47a30', '05ace8e3')
