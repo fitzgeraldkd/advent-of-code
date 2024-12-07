edc5e4bee07f5415aae17685302b770e0a2c8060
Kenny <kfonatoko@gmail.com>
2024-12-07 00:49:30 -0500

    Improve solution for 2024 day 7

--- solutions/year_2024/day_07/solution.py
+++ solutions/year_2024/day_07/solution.py
@@ -1,6 +1,4 @@
-from itertools import product
 from solutions import BaseSolution
-
 
 class Year2024Day07(BaseSolution):
     module_file = __file__
@@ -9,64 +7,48 @@ class Year2024Day07(BaseSolution):
         target, values = line.strip().split(": ")
         return [int(target), [int(v) for v in values.split(" ")]]
 
-    def can_validate(self, target, values):
-        operator_count = len(values) - 1
-        operator_combinations = product(["+", "*"], repeat=operator_count)
+    def check_operations(self, target, values, include_concat=False):
+        operators = ["+", "*"]
+        if include_concat:
+            operators.append("||")
 
-        # print(list(operator_combinations))
-        for combination in operator_combinations:
-            result, *remaining = values
-            for operator in combination:
-                operand, *remaining = remaining
-                if operator == "+":
-                    result += operand
+        for operator in operators:
+            a, b, *remaining = values
+
+            if operator == "+":
+                total = a + b
+            elif operator == "||":
+                total = int(f"{a}{b}")
+            else:
+                total = a * b
+
+            # Assumes there are no 0s in the values.
+            if total > target:
+                continue
+
+            if len(remaining) == 0:
+                if total == target:
+                    return True
                 else:
-                    result *= operand
-                if result > target:
-                    break
-            if result == target:
+                    continue
+
+            passed = self.check_operations(target, [total, *remaining], include_concat)
+            if passed:
                 return True
+
         return False
-
-    def can_validate_part_2(self, target, values):
-        operator_count = len(values) - 1
-        operator_combinations = product(["+", "*", "||"], repeat=operator_count)
-
-        checked_combinations = set()
-
-        for combination in operator_combinations:
-            result, *remaining = values
-            applied_operations = ""
-            for operator in combination:
-                applied_operations += operator
-                if applied_operations in checked_combinations:
-                    break
-                operand, *remaining = remaining
-                if operator == "+":
-                    result += operand
-                elif operator == "||":
-                    result = int(f"{result}{operand}")
-                else:
-                    result *= operand
-                if result > target:
-                    checked_combinations.add(applied_operations)
-                    break
-            if result == target:
-                return True
-        return False
-
 
     def part_1(self):
         results = 0
         for target, values in self.inputs:
-            if self.can_validate(target, values):
+            if self.check_operations(target, values):
                 results += target
         return results
 
     def part_2(self):
         results = 0
         for target, values in self.inputs:
-            if self.can_validate_part_2(target, values):
+            if self.check_operations(target, values, include_concat=True):
                 results += target
         return results
 
